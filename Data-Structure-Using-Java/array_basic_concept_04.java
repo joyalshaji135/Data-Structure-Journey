a79a1cf5644c13f9f4dac1841895f6d3d6ed784b
joyalshaji135 <joyalshaji135@gmail.com>
2024-03-15 10:39:02 +0530

    Array Basic

---
+++ Data-Structure-Using-Java/array_basic_concept_04.java
@@ -0,0 +1,25 @@
+// Array Basic Concepts
+
+public class array_basic_concept_04 {
+    public static void main(String[] args) {
+        // Array Declaration
+        int arr[]; // Basic Array Declaration
+        arr = new int[100];
+        int  [] arr1;  // Standard From
+        arr1 = new int[100];
+        // Array Initialization
+        int arr2[] = {10,20,30,40};
+        int [] arr3 = {10,20,30,40};
+    }
+}
+
+// Array Basic Operation in Data Structure
+
+/* 
+   1) init
+   2) set
+   3) get
+   4) traverse
+   5) insert
+   6) delete
+ */
